63fe0dd2dd73432077583c9575658ff535986e3b
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-17 05:45:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,5 +8,9 @@ end
 
 def sort_array_char_count (array)
   #array_char_length = array.length
-  array.sort { |a,b| a.length <=> b.length }
+  #array.sort { |a,b| a.length <=> b.length }
+  array[1], array[2] = array[2], array[1]
 end
+
+def swap_elements (array)
+  array.sort { |a,b| }
