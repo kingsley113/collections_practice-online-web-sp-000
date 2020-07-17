b90cf307d15dfc08dde55cbff623b071f2ddbbfb
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-17 05:38:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,8 @@ end
 def sort_array_desc (array)
   array.sort { |a,b| b <=> a}
 end
+
+def sort_array_char_count (array)
+  #array_char_length = array.length
+  array.sort { |a,b| a.length <=> b.length }
+end
